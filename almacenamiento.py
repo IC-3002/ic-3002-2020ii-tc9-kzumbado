d657752179d1136b7fe822871af609804e5a587c
kzumbado <61952168+kzumbado@users.noreply.github.com>
2020-12-02 17:01:40 -0600

    Tarea 9 Final

--- almacenamiento.py
+++ almacenamiento.py
@@ -1,2 +1,33 @@
+def quicksort(n):
+
+    size = len(n)
+    menores = []
+    mayores = []
+
+    if size == 1 or size == 0:
+        return n
+
+    else:
+        pivote = n[(size - 1) // 2]
+        pivoteComp = n[(size - 1) // 2][1]
+        for j in range(size):
+            if n[j][1] < pivoteComp:
+                menores.append(n[j])
+            elif n[j][1] > pivoteComp:
+                mayores.append(n[j])
+        
+        menores = quicksort(menores)
+        mayores = quicksort(mayores)
+        menores.append(pivote)
+
+        return menores + mayores
+
 def maximizar(As, D):
-    raise NotImplementedError()
+    As= quicksort(As)
+    actValor= 0
+    result = []
+    for i in range (0, len(As)):
+        if (actValor + As[i][1]) <= D:
+            result += [As[i]]
+            actValor += As[i][1]
+    return result
